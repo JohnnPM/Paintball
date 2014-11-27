f0b8846ddcaabfca72747dea0938062e850b0bd5
xXJarJarXx <598Johnn897@gmail.com>
2014-11-27 00:47:54 -0500

    Initial Commit

---
+++ src/paintball/Paintball.java
@@ -0,0 +1,31 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball
+ *
+ */
+package paintball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ */
+public class Paintball extends JavaPlugin
+{
+	@Override public void onLoad()
+	{
+		
+	}
+	
+	@Override public void onEnable()
+	{
+		
+	}
+	
+	@Override public void onDisable()
+	{
+		
+	}
+}
---
+++ src/paintball/lib/References.java
@@ -0,0 +1,16 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball.lib
+ *
+ */
+package paintball.lib;
+
+/**
+ * 
+ */
+public class References
+{
+
+}
