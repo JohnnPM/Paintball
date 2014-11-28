8d2764fa6474c8df1fc8455c2969fa0d8c1a2837
xXJarJarXx <598Johnn897@gmail.com>
2014-11-27 22:57:01 -0500

    Updated Progress 10:56PM EST 11/27/14

--- src/paintball/manager/PlayerManager.java
+++ src/paintball/manager/PlayerManager.java
@@ -1,6 +1,33 @@
 package paintball.manager;
 
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
 public class PlayerManager
 {
-
+	public PlayerManager()
+	{
+		
+	}
+	
+	private ArrayList<String> players = new ArrayList<String>();
+	
+	public void addPlayer(Player player) 
+	{
+		players.add(player.getName());
+	}
+	
+	public void removePlayer(Player player)
+	{
+		if (players.contains(player.getName()))
+			players.remove(player.getName());
+		else
+			return;
+	}
+	
+	public ArrayList<String> getPlayers()
+	{
+		return players;
+	}
 }
